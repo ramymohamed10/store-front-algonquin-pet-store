abe43f30a78dc62f17de1ebcd113b03db124ecf9
Ramy Mohamed <mohamer@algonquincollege.com>
2024-09-14 12:57:35 -0400

    Refactor the code to apply Factor 3: Configurations

--- src/components/OrderForm.vue
+++ src/components/OrderForm.vue
@@ -71,7 +71,7 @@ export default {
   methods: {
     async fetchProducts() {
       try {
-        const response = await fetch('http://localhost:3030/products');
+        const response = await fetch(`${process.env.VUE_APP_PRODUCT_SERVICE_URL}/products`);
         if (response.ok) {
           this.products = await response.json();
         } else {
@@ -89,7 +89,7 @@ export default {
       }
 
       try {
-        const response = await fetch('http://localhost:3000/orders', {
+        const response = await fetch(`${process.env.VUE_APP_ORDER_SERVICE_URL}/orders`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
